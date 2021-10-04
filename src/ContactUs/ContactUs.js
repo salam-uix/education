6691708801d738225c0545e072daae879a5c3e36
Abdus salam <salam.ads97@gmail.com>
2021-10-04 23:47:24 +0600

    contact page

--- src/ContactUs/ContactUs.js
+++ src/ContactUs/ContactUs.js
@@ -1,36 +1,47 @@
 import Button from '@restart/ui/esm/Button';
 import React from 'react';
-import { Form } from 'react-bootstrap';
+import { Form, Row } from 'react-bootstrap';
 import Footer from '../components/Footer/Footer';
 import MenuBar from '../components/MenuBar/MenuBar';
+import './ContactUs.css'
 
 const ContactUs = () => {
     return (
-        <div>
+        <div className="contact-container">
             <div className="bg-dark py-4 mb-5">
                 <MenuBar></MenuBar>
             </div>
-            <div>
-            <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
+            <img className="contact-page-img" src="https://images.unsplash.com/photo-1487017159836-4e23ece2e4cf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1471&q=80" alt="" />
+            <div className="container contact-container mt-5 mb-5">
+                <Row xs={1} md={2} className="g-4">
+                    <div>
+                        <h1>Get In Touch</h1>
+                        <p>If you would like to find out more about how we can help you, please give us a call or drop us an email. We welcome your comments and suggestions about this website and/or any other issues that you wish to raise.</p>
+                    </div>
+                    <div>
+                        <Form>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Your name</Form.Label>
+                                <Form.Control type="text" placeholder="Enter your name" />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" />
+                                <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                                </Form.Text>
+                            </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-                </Form>
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                                <Form.Label>Example textarea</Form.Label>
+                                <Form.Control as="textarea" rows={3} />
+                            </Form.Group>
+                            <Button className="contact-us-btn" variant="primary" type="submit">
+                                Submit
+                            </Button>
+                        </Form>
+                    </div>
+                </Row>
             </div>
             <Footer></Footer>
         </div>
